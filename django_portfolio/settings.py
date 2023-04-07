e59e64a61dad06a9911be2d8227bc0efee0e79c4
Jherson Miguel <90340841+JherMik7@users.noreply.github.com>
2023-04-07 18:14:05 -0500

    make it better

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/public/'
 
@@ -126,3 +125,6 @@ MEDIA_URL = '/public/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
